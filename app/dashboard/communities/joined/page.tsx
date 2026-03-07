2e648deea5437c229f9d0f7160da69b018b0b217
Adrian Sanchez <adrian.s.duque@gmail.com>
2026-03-07 18:52:39 -0400

    creacion de las paginas de crear comunidad y comunidades a las que te uniste

---
+++ app/dashboard/communities/joined/page.tsx
@@ -0,0 +1,25 @@
+import Heading from "@/components/typography/Heading";
+import generatePageTitle from "@/src/shared/utils/metadata";
+import { Metadata } from "next";
+import Link from "next/link";
+
+const title = 'Comunidades a las que te uniste'
+
+export const metadata: Metadata = {
+    title: generatePageTitle(title)
+}
+
+export default function JoinedCommunitiesPage() {
+    return (
+        <>
+            <Heading>{title}</Heading>
+
+            <div className="flex justify-between flex-col lg:flex-row">
+                <Link
+                    href="/dashboard/communities"
+                    className="mt-5 block lg:inline-block text-center bg-orange-500 hover:bg-orange-600 transition-colors text-xs lg:text-xl text-white py-3 px-10  font-bold"
+                >Volver a mis Comunidades</Link>
+            </div>
+        </>
+    );
+}
