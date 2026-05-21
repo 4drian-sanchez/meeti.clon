8498a145ae96f3e3146603ad9e431f2d5d4a0e55
Adrian Sanchez <adrian.s.duque@gmail.com>
2026-05-20 21:23:35 -0400

    agregar carga de comunidades en el formulario con manejo de estado y visualización en tiempo de espera

--- src/features/meetis/CommunitiesFormField.tsx
+++ src/features/meetis/CommunitiesFormField.tsx
@@ -1,11 +1,20 @@
 import { FormLabel, FormSelect } from "@/src/shared/components/forms";
+import { Suspense, use } from "react";
+
+const communitiesPromise = fetch('/api/communities').then(res => res.json())
 
 function CommunitiesOptions () {
+
+    const communities = use<{id: string, name: string}[]>( communitiesPromise )
+
     return (
         <>
             <FormLabel>Comunidades meeti</FormLabel>
             <FormSelect>
                 <option value="">Seleccione una comunidad</option>
+                {communities.map( community => (
+                  <option key={community.id} value={community.id}>{community.name}</option>
+                ) )}
             </FormSelect>
         </>
     )
@@ -13,8 +22,8 @@ function CommunitiesOptions () {
 
 export default function CommunitiesFormField() {
   return (
-    <>
-      <CommunitiesOptions />
-    </>
+      <Suspense fallback={'cargando...'}>
+        <CommunitiesOptions />
+      </Suspense>
   );
 }
