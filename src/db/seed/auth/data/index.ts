b59a32d4b1aeff4756c1b61306dfe6d09a0325f8
Adrian Sanchez <adrian.s.duque@gmail.com>
2026-05-29 18:18:10 -0400

    seed de auth modularizado: divición de data y consulta de la db

--- src/db/seed/auth/data/index.ts
+++ src/db/seed/auth/data/index.ts
@@ -1,10 +1,45 @@
-import type { RawUser } from "../types";
+import { hashPassword } from "better-auth/crypto";
+import type { AccountSeed, RawUser, UserSeed } from "../types";
 
-export const rawUsers: RawUser[] = [
-  { name: "Alice Johnson", email: "alice@example.com" },
-  { name: "Bob Smith", email: "bob@example.com" },
-  { name: "Carlos Méndez", email: "carlos@example.com" },
-  { name: "Diana Prince", email: "diana@example.com" },
-  { name: "Evan Wright", email: "evan@example.com" },
-  { name: "Adrian Sanchez", email: "adrian@example.com" },
-];
+export async function authSeedData() : Promise<{mockUsers: UserSeed[], mockAccounts: AccountSeed[]}> {
+
+    const hashedPassword = await hashPassword('password123');
+
+    const rawUsers: RawUser[] = [
+      { name: "Adrian Sanchez", email: "adrian@example.com" },
+        { name: "Alice Johnson", email: "alice@example.com" },
+        { name: "Bob Smith", email: "bob@example.com" },
+        { name: "Carlos Méndez", email: "carlos@example.com" },
+        { name: "Diana Prince", email: "diana@example.com" },
+        { name: "Evan Wright", email: "evan@example.com" },
+    ];
+
+
+    const mockUsers: UserSeed[] = [];
+    const mockAccounts: AccountSeed[] = [];
+
+    for (const {name, email} of rawUsers) {
+        const userId = crypto.randomUUID();
+
+        mockUsers.push({
+            id: userId,
+            name: name,
+            email: email,
+            emailVerified: true,
+            image: null,
+        })
+
+        mockAccounts.push({
+            id: crypto.randomUUID(),
+            userId: userId,
+            accountId: email,
+            providerId: "credential",
+            password: hashedPassword,
+        });
+    }
+
+    return {
+      mockUsers,
+      mockAccounts
+    }
+}
--- src/db/seed/auth/index.ts
+++ src/db/seed/auth/index.ts
@@ -1,57 +1,22 @@
-import 'dotenv/config'
-import { hashPassword } from "better-auth/crypto";
-import type { AccountSeed, RawUser, UserSeed } from './types'
+import 'dotenv/config';
 import { accounts, users } from "../../schemas";
 import { drizzle } from "drizzle-orm/node-postgres";
-
+import { authSeedData } from './data';
 
 async function authSeeder() {
+    const { mockAccounts, mockUsers } = await authSeedData()
 
-    const hashedPassword = await hashPassword('password123');
-
-    const rawUsers: RawUser[] = [
-        { name: "Alice Johnson", email: "alice@example.com" },
-        { name: "Bob Smith", email: "bob@example.com" },
-        { name: "Carlos Méndez", email: "carlos@example.com" },
-        { name: "Diana Prince", email: "diana@example.com" },
-        { name: "Evan Wright", email: "evan@example.com" },
-        { name: "Adrian Sanchez", email: "adrian@example.com" },
-    ];
-
-
-    const mockUsers: UserSeed[] = [];
-    const mockAccounts: AccountSeed[] = [];
-
-    for (const u of rawUsers) {
-        const userId = crypto.randomUUID();
-
-        mockUsers.push({
-            id: userId,
-            name: u.name,
-            email: u.email,
-            emailVerified: true,
-            image: null,
-        })
-
-        mockAccounts.push({
-            id: crypto.randomUUID(),
-            userId: userId,
-            accountId: u.email,
-            providerId: "credential",
-            password: hashedPassword,
-        });
-    }
-
-    //* Insertar en la DB
     try {
-
         const db = drizzle(process.env.DB_URL!)
-
         await db.insert(users).values(mockUsers)
         await db.insert(accounts).values(mockAccounts)
+
+        console.log('El seed de auth se ha ejecutado con exito')
+        process.exit(0)
     } catch (error) {
-        console.log(error)
+        console.log('Hubo un error al ejecutar el seed de auth',error)
+        process.exit(1)
     }
 }
 
-authSeeder()
+if(process.env.RUN_SEEDS === 'true') authSeeder()
